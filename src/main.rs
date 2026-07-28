88dcf5fc4f4e7b42b569188ae058dd1363cc1d93
R2 <s1f102503183@iniad.org>
2026-07-28 21:13:58 +0900

    add grid and display

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,58 @@
-use macroquad::prelude::*;
+use macroquad::{miniquad::window::set_window_size, prelude::*};
 
 enum GameState {
     Title,
-    InGame { grid: [[u8; 10]; 20] },
+    Playing { grid: [[u8; 10]; 20] },
     GameOver,
 }
 
 #[macroquad::main("tetris")]
 async fn main() {
+    set_window_size(400, 500);
     let mut state = GameState::Title;
+    const BLOCK_SIZE: f32 = 20.;
+
     loop {
         clear_background(BLACK);
         match state {
             GameState::Title => {
                 draw_text("TETRIS", 100., 100., 40., WHITE);
                 if is_key_pressed(KeyCode::Space) {
-                    state = GameState::InGame {
+                    state = GameState::Playing {
                         grid: [[0; 10]; 20],
                     };
                 }
             }
-            GameState::InGame { grid } => {
+            GameState::Playing { mut grid } => {
                 // logic
+                grid[19][0] = 1;
 
                 // draw
+                for y in 0..grid.len() {
+                    for x in 0..grid[y].len() {
+                        match grid[y][x] {
+                            1 => {
+                                draw_rectangle(
+                                    x as f32 * BLOCK_SIZE + (screen_width() / 4.),
+                                    y as f32 * BLOCK_SIZE + 50.,
+                                    BLOCK_SIZE,
+                                    BLOCK_SIZE,
+                                    WHITE,
+                                );
+                            }
+                            _ => {
+                                draw_rectangle_lines(
+                                    x as f32 * BLOCK_SIZE + (screen_width() / 4.),
+                                    y as f32 * BLOCK_SIZE + 50.,
+                                    BLOCK_SIZE,
+                                    BLOCK_SIZE,
+                                    2.,
+                                    WHITE,
+                                );
+                            }
+                        }
+                    }
+                }
             }
             GameState::GameOver => {}
         }
